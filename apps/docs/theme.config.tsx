0c5459a1e79fcbd794702ffafaf4156284a24bc1
Gabriel <gabrielsmenezes99@gmail.com>
2023-06-19 18:59:58 -0300

    chore: :building_construction: setup nextra

---
+++ apps/docs/theme.config.tsx
@@ -0,0 +1,14 @@
+import { DocsThemeConfig } from 'nextra-theme-docs';
+import React from 'react';
+
+const config: DocsThemeConfig = {
+  logo: <span>My Project</span>,
+  project: {
+    link: 'https://github.com/mnzsss/react-chakra-pagination',
+  },
+  footer: {
+    text: 'React Chakra Pagination',
+  },
+};
+
+export default config;
